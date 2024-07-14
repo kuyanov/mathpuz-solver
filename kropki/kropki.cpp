6d80620d82fca771c03acec123e2b55b44ae9a12
Fedor Kuyanov <feodor.kuyanov@gmail.com>
2024-07-14 11:20:59 +0300

    Improved kropki solver

--- kropki/kropki.cpp
+++ kropki/kropki.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+int getvar(int n, int i, int j, int d) {
+    return 1 + i * n * n + j * n + d;
+}
+
 int main() {
     int n;
     cin >> n;
@@ -29,11 +33,11 @@ int main() {
         for (int j = 0; j < n; j++) {
             cnf.emplace_back();
             for (int d = 0; d < n; d++) {
-                cnf.back().push_back(1 + i * n * n + j * n + d);
+                cnf.back().push_back(getvar(n, i, j, d));
             }
             for (int d1 = 0; d1 < n; d1++) {
                 for (int d2 = d1 + 1; d2 < n; d2++) {
-                    cnf.push_back({-(1 + i * n * n + j * n + d1), -(1 + i * n * n + j * n + d2)});
+                    cnf.push_back({-getvar(n, i, j, d1), -getvar(n, i, j, d2)});
                 }
             }
         }
@@ -42,7 +46,7 @@ int main() {
         for (int d = 0; d < n; d++) {
             cnf.emplace_back();
             for (int j = 0; j < n; j++) {
-                cnf.back().push_back(1 + i * n * n + j * n + d);
+                cnf.back().push_back(getvar(n, i, j, d));
             }
         }
     }
@@ -50,74 +54,69 @@ int main() {
         for (int d = 0; d < n; d++) {
             cnf.emplace_back();
             for (int i = 0; i < n; i++) {
-                cnf.back().push_back(1 + i * n * n + j * n + d);
+                cnf.back().push_back(getvar(n, i, j, d));
             }
         }
     }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - 1; j++) {
-            if (hor[i][j] == 1) {
-                for (int d = 0; d < n; d++) {
-                    cnf.push_back({-(1 + i * n * n + j * n + d)});
-                    if (d > 0) cnf.back().push_back(1 + i * n * n + (j + 1) * n + d - 1);
-                    if (d < n - 1) cnf.back().push_back(1 + i * n * n + (j + 1) * n + d + 1);
-                }
-            } else if (hor[i][j] == 2) {
-                for (int d = 0; d < n; d++) {
-                    cnf.push_back({-(1 + i * n * n + j * n + d)});
-                    if (d % 2 == 1) cnf.back().push_back(1 + i * n * n + (j + 1) * n + (d - 1) / 2);
-                    if (2 * (d + 1) <= n) cnf.back().push_back(1 + i * n * n + (j + 1) * n + 2 * d + 1);
-                }
-            } else {
-                for (int d = 0; d < n; d++) {
-                    cnf.push_back({-(1 + i * n * n + j * n + d)});
-                    for (int d2 = 0; d2 < n; d2++) {
-                        if (d2 != d - 1 && d2 != d + 1 && (d % 2 == 0 || d2 != (d - 1) / 2) && d2 != 2 * d + 1) {
-                            cnf.back().push_back(1 + i * n * n + (j + 1) * n + d2);
-                        }
-                    }
+            for (int d = 0; d < n; d++) {
+                int v0 = getvar(n, i, j, d);
+                int v1 = d > 0 ? getvar(n, i, j + 1, d - 1) : -1;
+                int v2 = d < n - 1 ? getvar(n, i, j + 1, d + 1) : -1;
+                int v3 = d % 2 == 1 ? getvar(n, i, j + 1, (d - 1) / 2) : -1;
+                int v4 = 2 * (d + 1) <= n ? getvar(n, i, j + 1, 2 * d + 1) : -1;
+                if (hor[i][j] == 1) {
+                    cnf.push_back({-v0});
+                    if (v1 != -1) cnf.back().push_back(v1);
+                    if (v2 != -1) cnf.back().push_back(v2);
+                } else if (hor[i][j] == 2) {
+                    cnf.push_back({-v0});
+                    if (v3 != -1) cnf.back().push_back(v3);
+                    if (v4 != -1) cnf.back().push_back(v4);
+                } else {
+                    if (v1 != -1) cnf.push_back({-v0, -v1});
+                    if (v2 != -1) cnf.push_back({-v0, -v2});
+                    if (v3 != -1) cnf.push_back({-v0, -v3});
+                    if (v4 != -1) cnf.push_back({-v0, -v4});
                 }
             }
         }
     }
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n; j++) {
-            if (vert[i][j] == 1) {
-                for (int d = 0; d < n; d++) {
-                    cnf.push_back({-(1 + i * n * n + j * n + d)});
-                    if (d > 0) cnf.back().push_back(1 + (i + 1) * n * n + j * n + d - 1);
-                    if (d < n - 1) cnf.back().push_back(1 + (i + 1) * n * n + j * n + d + 1);
-                }
-            } else if (vert[i][j] == 2) {
-                for (int d = 0; d < n; d++) {
-                    cnf.push_back({-(1 + i * n * n + j * n + d)});
-                    if (d % 2 == 1) cnf.back().push_back(1 + (i + 1) * n * n + j * n + (d - 1) / 2);
-                    if (2 * (d + 1) <= n) cnf.back().push_back(1 + (i + 1) * n * n + j * n + 2 * d + 1);
-                }
-            } else {
-                for (int d = 0; d < n; d++) {
-                    cnf.push_back({-(1 + i * n * n + j * n + d)});
-                    for (int d2 = 0; d2 < n; d2++) {
-                        if (d2 != d - 1 && d2 != d + 1 && (d % 2 == 0 || d2 != (d - 1) / 2) && d2 != 2 * d + 1) {
-                            cnf.back().push_back(1 + (i + 1) * n * n + j * n + d2);
-                        }
-                    }
+            for (int d = 0; d < n; d++) {
+                int v0 = getvar(n, i, j, d);
+                int v1 = d > 0 ? getvar(n, i + 1, j, d - 1) : -1;
+                int v2 = d < n - 1 ? getvar(n, i + 1, j, d + 1) : -1;
+                int v3 = d % 2 == 1 ? getvar(n, i + 1, j, (d - 1) / 2) : -1;
+                int v4 = 2 * (d + 1) <= n ? getvar(n, i + 1, j, 2 * d + 1) : -1;
+                if (vert[i][j] == 1) {
+                    cnf.push_back({-v0});
+                    if (v1 != -1) cnf.back().push_back(v1);
+                    if (v2 != -1) cnf.back().push_back(v2);
+                } else if (vert[i][j] == 2) {
+                    cnf.push_back({-v0});
+                    if (v3 != -1) cnf.back().push_back(v3);
+                    if (v4 != -1) cnf.back().push_back(v4);
+                } else {
+                    if (v1 != -1) cnf.push_back({-v0, -v1});
+                    if (v2 != -1) cnf.push_back({-v0, -v2});
+                    if (v3 != -1) cnf.push_back({-v0, -v3});
+                    if (v4 != -1) cnf.push_back({-v0, -v4});
                 }
             }
         }
     }
     vector<bool> sol(n * n * n);
     if (satisfiable(cnf, sol)) {
-        cout << "SAT" << endl;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 auto start = sol.begin() + i * n * n + j * n;
-                int d = find(start, start + n, true) - start;
+                size_t d = find(start, start + n, true) - start;
                 cout << d + 1 << ' ';
             }
             cout << endl;
         }
-    } else {
-        cout << "UNSAT" << endl;
     }
 }
